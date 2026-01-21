81e6d89ecd7f295f8529bb4f968e1e9952ec383b
timxxx-cpu <timopenbox@gmail.com>
2026-01-21 13:18:42 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { execSync } from 'child_process'
 import packageJson from './package.json'
 
-// Безопасная функция получения версии
+// Безопасная функция получения 
 const getVersionInfo = () => {
   try {
     // Пытаемся получить хеш из Git
