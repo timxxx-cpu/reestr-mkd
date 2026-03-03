83b9f4e92b4365267f88aba85b7c21a17ade1fa6
timxxx-cpu <timopenbox@gmail.com>
2026-03-04 02:02:48 +0500

    remove Java JPA parity test result artifact

--- apps/backend-java-jpa/src/main/java/uz/reestrmkd/backendjpa/repo/UnitRepository.java
+++
@@ -1,8 +0,0 @@
-// UnitRepository.java
-package uz.reestrmkd.backendjpa.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.reestrmkd.backendjpa.domain.UnitEntity;
-import java.util.List;
-public interface UnitRepository extends JpaRepository<UnitEntity, String> {
-    List<UnitEntity> findByFloorIdIn(List<String> floorIds);
-}
